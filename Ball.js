06d43a2d062712287e2bfa353bb5952219b7cd09
Daniel Golawski <dangolawski@gmail.com>
2020-11-01 22:54:38 +0100

    jumping has been implemented

--- Ball.js
+++ Ball.js
@@ -19,6 +19,9 @@ class Ball {
     }
     mapManager;
 
+    isJumping = false;
+    isFalling = false;
+
     constructor(context, mapManager) {
         this.context = context;
         this.mapManager = mapManager;
@@ -41,17 +44,22 @@ class Ball {
     }
 
     updateMovement() {
-        return this.movement = this.mapManager.checkTouchedTilesPermeability(this.x, this.y, this.radius);
+        this.movement = this.mapManager.checkTouchedTilesPermeability(this.x, this.y, this.radius);
     }
 
     updatePosition() {
-        if (this.movement.down) {
-            let gravity = this.gravity;
-            while (gravity > 0 && this.movement.down) {
-                this.y += 1;
-                gravity -= 1;
-                this.movement = this.updateMovement();
-            }
+        if (this.movement.down && !this.isJumping && !this.isFalling) {
+            // let gravity = this.gravity;
+            // while (gravity > 0 && this.movement.down) {
+            //     this.y += 1;
+            //     gravity -= 1;
+            //     this.updateMovement();
+            // }
+            this.handleFalling();
+        }
+
+        if (this.movement.up && this.keysPressed.up && !this.isJumping && !this.movement.down) {
+            this.handleJumping();
         }
 
         let distance = this.speed;
@@ -60,14 +68,14 @@ class Ball {
             while (distance > 0 && this.movement.left) {
                 this.x -= 1;
                 distance -= 1;
-                this.movement = this.updateMovement();
+                this.updateMovement();
             }
         }
         if (this.keysPressed.right) {
             while (distance > 0 && this.movement.right) {
                 this.x += 1;
                 distance -= 1;
-                this.movement = this.updateMovement();
+                this.updateMovement();
             }
         }
     }
@@ -91,5 +99,43 @@ class Ball {
         if (key.key === 'ArrowUp') {
             this.keysPressed.up = keydown;
         }
+    }
+
+    handleJumping() {
+        this.isJumping = true;
+        let jumpForce = 2 * this.gravity;
+        const jumpInterval = setInterval(() => {
+            let counter = jumpForce;
+            while (counter > 0 && this.movement.up) {
+                this.y -= 1;
+                counter--;
+                this.updateMovement();
+            }
+            jumpForce -= 1;
+            if (jumpForce <= 0) {
+                clearInterval(jumpInterval);
+                this.isJumping = false;
+            }
+        }, 20);
+    }
+
+    handleFalling() {
+        this.isFalling = true;
+        let gravity = 1;
+        const fallInterval = setInterval(() => {
+            let counter = gravity;
+            while (counter > 0 && this.movement.down) {
+                this.y += 1;
+                counter--;
+                this.updateMovement();
+            }
+            if (gravity < this.gravity) {
+                gravity++;
+            }
+            if (!this.movement.down) {
+                clearInterval(fallInterval);
+                this.isFalling = false;
+            }
+        }, 20);
     }
 }
